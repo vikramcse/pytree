cb148c4c21cd84e58d469809baf0f91969e0f976
Vikram Jadhav <vikramcse.10@gmail.com>
2018-07-02 17:46:24 +0530

    file tree implementation in python

---
+++ tree.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+args = ['.']
+if len(sys.argv) > 1:
+    args = sys.argv[1:]
+
+
+def tree(arg, indent=""):
+    path = os.path.abspath(arg)
+    print os.path.basename(path)
+
+    if not os.path.isdir(path):
+        return
+
+    current_dirs = os.listdir(arg)
+    filtered_names = []
+
+    for f in current_dirs:
+        if not f.startswith('.'):
+            filtered_names.append(f)
+
+    filtered_names = sorted(filtered_names)
+    for i, d in enumerate(filtered_names):
+        add = "│  "
+        if i == len(filtered_names) - 1:
+            print indent + "└──",
+            add = "   "
+        else:
+            print indent + "├──",
+
+        tree(os.path.join(arg, d), indent=indent + add)
+
+
+for arg in args:
+    try:
+        tree(arg)
+    except Exception as e:
+        print e.message
